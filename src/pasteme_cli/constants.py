ec77196f11df5a5390f34cf5697d799a07d23771
sadra <sadra@dopely.top>
2022-07-10 19:46:13 +0430

    configutations added

---
+++ src/pasteme_cli/constants.py
@@ -0,0 +1,20 @@
+LANGUAGES = (
+    ('bash', 'Bash'),
+    ('c', 'C'),
+    ('cpp', 'C++'),
+    ('csharp', 'C#'),
+    ('css', 'CSS'),
+    ('go', 'Go'),
+    ('html', 'HTML'),
+    ('java', 'Java'),
+    ('js', 'JavaScript'),
+    ('json', 'JSON'),
+    ('lua', 'Lua'),
+    ('md', 'markdown'),
+    ('php', 'PHP'),
+    ('plaintext', 'PlainText'),
+    ('python', 'Python'),
+    ('rb', 'Ruby'),
+)
+
+PASTEME_SERVICE_URL = 'https://pasteme.pythonanywhere.com'
