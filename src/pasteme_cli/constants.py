f4a02bf10ac4c68ce0c4881fadfc596f5eeba07d
sadra <sadra@dopely.top>
2022-07-11 14:57:38 +0430

    available languages added

--- src/pasteme_cli/constants.py
+++ src/pasteme_cli/constants.py
@@ -1,20 +1,33 @@
-LANGUAGES = (
-    ('bash', 'Bash'),
-    ('c', 'C'),
-    ('cpp', 'C++'),
-    ('csharp', 'C#'),
-    ('css', 'CSS'),
-    ('go', 'Go'),
-    ('html', 'HTML'),
-    ('java', 'Java'),
-    ('js', 'JavaScript'),
-    ('json', 'JSON'),
-    ('lua', 'Lua'),
-    ('md', 'markdown'),
-    ('php', 'PHP'),
-    ('plaintext', 'PlainText'),
-    ('python', 'Python'),
-    ('rb', 'Ruby'),
-)
+LANGUAGES = {
+    'bash': 'Command Language',
+    'c': 'C Language',
+    'cpp': 'C++ Language',
+    'csharp': 'C# Language',
+    'css': 'CSS Style Sheet Language',
+    'go': 'Go Language',
+    'html': 'HTML Language',
+    'java': 'Java Language',
+    'js': 'JavaScript Language',
+    'json': 'JSON Data Format',
+    'lua': 'Lua Language',
+    'md': 'MarkDown Language',
+    'php': 'PHP Language',
+    'plaintext': 'PlainText',
+    'python': 'Python Language',
+    'rb': 'Ruby Language'
+}
+
+LANGUAGES_HINT = f'''snippet language (available languages:
+{", ".join([l for l in LANGUAGES.keys()])})
+'''
 
 PASTEME_SERVICE_URL = 'https://pasteme.pythonanywhere.com'
+
+PASTEME_API_URL = 'http://localhost:8000/api/v1/paste/'
+
+CONNECTION_ISSUE_HINT = f'''Check your network connection. Make sure the PasteMe service ({PASTEME_SERVICE_URL}) is up and running.
+If the issue still presists, contact mailto:lnxpylnxpy@gmail.com for more information.'''
+
+JSON_TEMPLATE = '''~~ {} ~~
+{}
+'''
