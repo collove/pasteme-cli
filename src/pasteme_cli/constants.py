a0208ba78d1022649275edf5b60e809a424c5ccd
Sadra <lnxpylnxpy@gmail.com>
2022-07-18 20:53:02 +0430

    JSON_TEMPLATE moved to pasteme sdk

--- src/pasteme_cli/constants.py
+++ src/pasteme_cli/constants.py
@@ -25,18 +25,8 @@ PASTEME_SERVICE_URL = 'https://pasteme.pythonanywhere.com'
 
 PASTEME_API_URL = 'https://pasteme.pythonanywhere.com/api/v1/paste/'
 
-CONNECTION_ISSUE_HINT = f'Check your network connection. Make sure the PasteMe service ({PASTEME_SERVICE_URL}) is up and running.'
-
-JSON_TEMPLATE = '''~~ {} ~~
-{}
-'''
+CONNECTION_ISSUE_HINT = 'Check your network connection. Make sure the PasteMe service is up and running.'
 
 EPILOG_DESCRIPTION = '''Use --start (-s) and/or --end (-e) to select a range of lines from the source code to be pasted.
 Author -> Sadra Yahyapour (mailto:lnxpylnxpy@gmail.com)
 GitHub -> https://github.com/collove/pasteme-cli'''
-
-
-try:
-    from .local_settings import *
-except:
-    pass
