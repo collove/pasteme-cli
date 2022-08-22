66cd764efd4c209798b7c8f8f7bf4b0eff3f8d2d
Hasan Ramezani <hasan.r67@gmail.com>
2022-08-22 19:00:28 +0200

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -66,7 +65,7 @@ setup(
         'tool',
         'linux',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'requests==2.28.1',
         'Pygments==2.12.0',
