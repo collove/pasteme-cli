f1abf7bcf80f76f4e6a752e229beed396ba7222b
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-10-29 10:53:13 +0330

    Bump version: 1.0.4 → 1.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ project = 'pasteme-cli'
 year = '2022'
 author = 'Sadra Yahyapour'
 copyright = f'{year}, {author}'
-version = release = '1.0.4'
+version = release = '1.0.5'
 
 pygments_style = 'trac'
 templates_path = ['.']
