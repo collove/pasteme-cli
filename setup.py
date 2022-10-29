72835c0caf263532f4c1a23f43d19c8120798c19
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-10-29 11:34:48 +0330

    Bump version: 1.0.5 → 1.0.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*names, **kwargs):
 
 setup(
     name='pasteme-cli',
-    version='1.0.5',
+    version='1.0.6',
     license='MIT',
     description='A CLI pastebin tool.',
     long_description='{}\n\n{}'.format(
